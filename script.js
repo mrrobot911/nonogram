e3dd51856336c7d53595f15f009d3a107ee79f45
mrrobot911 <kelwin2705@gmail.com>
2024-02-01 23:33:37 +0300

    fix: canvas grid owerflow

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import drawGrid from './helpers/drawgrid.js';
 
-const CELL_COUNT = 20;
+const CELL_COUNT = 15;
 const CANVAS_SIZE = 600;
 const TITLE_SIZE = 70;
 const CELL_SIZE = CANVAS_SIZE / CELL_COUNT;
@@ -18,9 +18,9 @@ canvasContainer.append(canvas);
 
 const context = canvas.getContext('2d');
 
-for (let i = 0; i < canvas.width / CELL_SIZE; i += 1) {
+for (let i = 0; i < (canvas.width - TITLE_SIZE) / CELL_SIZE; i += 1) {
   gridColors[i] = [];
-  for (let j = 0; j < canvas.height / CELL_SIZE; j += 1) {
+  for (let j = 0; j < (canvas.height - TITLE_SIZE) / CELL_SIZE; j += 1) {
     gridColors[i][j] = '#ffffff';
   }
 }
@@ -32,12 +32,12 @@ function handleMouseClick(event) {
   const gridX = Math.floor((mouseX - TITLE_SIZE) / CELL_SIZE);
   const gridY = Math.floor((mouseY - TITLE_SIZE) / CELL_SIZE);
 
-  gridColors[gridX][gridY] =
-    gridColors[gridX][gridY] === '#ffffff' ? '#ff0000' : '#ffffff';
-
+  if (gridX >= 0 && gridY >= 0) {
+    gridColors[gridX][gridY] =
+      gridColors[gridX][gridY] === '#ffffff' ? '#000000' : '#ffffff';
+  }
   drawGrid(canvas, context, CELL_SIZE, gridColors, TITLE_SIZE);
 }
-
 canvas.addEventListener('click', handleMouseClick);
 
 drawGrid(canvas, context, CELL_SIZE, gridColors, TITLE_SIZE);
