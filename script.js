2d183e29bb37d8791cb007629507f5de78017c76
mrrobot911 <kelwin2705@gmail.com>
2024-02-02 15:36:47 +0300

    fix: change name of identifier

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ import isMatrixEqual from './helpers/equal.js';
 import arr from './helpers/solution.js';
 
 const CELL_COUNT = 5;
-let CANVAS_SIZE = window.innerWidth < 700 ? 400 : 600;
-let CELL_SIZE = CANVAS_SIZE / CELL_COUNT;
+let canvasSize = window.innerWidth < 700 ? 400 : 600;
+let cellSize = canvasSize / CELL_COUNT;
 const TITLE_SIZE = 70;
 const gridColors = [];
 
@@ -14,17 +14,17 @@ document.body.appendChild(canvasContainer);
 
 const canvas = document.createElement('canvas');
 canvas.id = 'gridCanvas';
-canvas.width = CANVAS_SIZE + TITLE_SIZE;
-canvas.height = CANVAS_SIZE + TITLE_SIZE;
+canvas.width = canvasSize + TITLE_SIZE;
+canvas.height = canvasSize + TITLE_SIZE;
 canvasContainer.append(canvas);
 
 const context = canvas.getContext('2d');
 function resizeCanvas(size) {
-  CANVAS_SIZE = size;
-  CELL_SIZE = CANVAS_SIZE / CELL_COUNT;
-  canvas.width = CANVAS_SIZE + TITLE_SIZE;
-  canvas.height = CANVAS_SIZE + TITLE_SIZE;
-  drawGrid(canvas, context, CELL_SIZE, gridColors, TITLE_SIZE, arr);
+  canvasSize = size;
+  cellSize = canvasSize / CELL_COUNT;
+  canvas.width = canvasSize + TITLE_SIZE;
+  canvas.height = canvasSize + TITLE_SIZE;
+  drawGrid(canvas, context, cellSize, gridColors, TITLE_SIZE, arr);
 }
 function updateCanvasSize() {
   if (window.innerWidth <= 400) {
@@ -41,9 +41,9 @@ function updateCanvasSize() {
   }
 }
 
-for (let i = 0; i < (canvas.width - TITLE_SIZE) / CELL_SIZE; i += 1) {
+for (let i = 0; i < (canvas.width - TITLE_SIZE) / cellSize; i += 1) {
   gridColors[i] = [];
-  for (let j = 0; j < (canvas.height - TITLE_SIZE) / CELL_SIZE; j += 1) {
+  for (let j = 0; j < (canvas.height - TITLE_SIZE) / cellSize; j += 1) {
     gridColors[i][j] = 0;
   }
 }
@@ -52,17 +52,17 @@ function handleMouseClick(event) {
   const mouseX = event.clientX - canvas.getBoundingClientRect().left;
   const mouseY = event.clientY - canvas.getBoundingClientRect().top;
 
-  const gridX = Math.floor((mouseX - TITLE_SIZE) / CELL_SIZE);
-  const gridY = Math.floor((mouseY - TITLE_SIZE) / CELL_SIZE);
+  const gridX = Math.floor((mouseX - TITLE_SIZE) / cellSize);
+  const gridY = Math.floor((mouseY - TITLE_SIZE) / cellSize);
 
   if (gridX >= 0 && gridY >= 0) {
     gridColors[gridX][gridY] = gridColors[gridX][gridY] === 0 ? 1 : 0;
   }
-  drawGrid(canvas, context, CELL_SIZE, gridColors, TITLE_SIZE, arr);
+  drawGrid(canvas, context, cellSize, gridColors, TITLE_SIZE, arr);
   const win = isMatrixEqual(gridColors, arr);
   if (win) alert('You win!');
 }
 canvas.addEventListener('click', handleMouseClick);
 window.addEventListener('resize', updateCanvasSize);
 
-drawGrid(canvas, context, CELL_SIZE, gridColors, TITLE_SIZE, arr);
+drawGrid(canvas, context, cellSize, gridColors, TITLE_SIZE, arr);
