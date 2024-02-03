7127a7e37b58eb4f59c64e23e94288fd5f280921
mrrobot911 <kelwin2705@gmail.com>
2024-02-03 21:49:09 +0300

    refuct: change solutions structure

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ import isMatrixEqual from './helpers/equal.js';
 import solutions from './helpers/solution.js';
 
 let canvasSize;
-const TITLE_SIZE = 70;
+let cellSize;
+let titleSize = 70;
 const gridColors = [];
-const cellCount = 5;
-let solutionArr = solutions.empty;
+let cellCount = 5;
+let solutionArr = solutions.empty[0].empty;
 let gameBegin = false;
+const lvl = ['easy', 'medium', 'hard'];
+let matrix = 'empty 0';
 
 const canvasContainer = document.createElement('section');
 canvasContainer.className = 'canvasContainer';
@@ -15,31 +18,61 @@ document.body.appendChild(canvasContainer);
 
 const canvas = document.createElement('canvas');
 canvas.id = 'gridCanvas';
-canvas.width = canvasSize + TITLE_SIZE;
-canvas.height = canvasSize + TITLE_SIZE;
+canvas.width = canvasSize + titleSize;
+canvas.height = canvasSize + titleSize;
 const controlsContainer = document.createElement('div');
 controlsContainer.className = 'controlsContainer';
 const restartBtn = document.createElement('button');
 restartBtn.textContent = 'Resturt Game';
 const playBtn = document.createElement('button');
 playBtn.textContent = 'Start Game';
-const templateSelect = document.createElement('select');
-const defaultOption = document.createElement('option');
-defaultOption.value = '';
-defaultOption.text = 'Easy';
-defaultOption.selected = true;
-defaultOption.disabled = true;
-templateSelect.append(defaultOption);
-const templates = solutions.easy;
 
-templates.forEach((_, index) => {
-  const option = document.createElement('option');
-  option.value = `easy ${index}`;
-  option.text = index + 1;
-  templateSelect.append(option);
+controlsContainer.append(playBtn, restartBtn);
+
+const templateSelect1 = document.createElement('select');
+const templateSelect2 = document.createElement('select');
+const templateSelect3 = document.createElement('select');
+const selectors = [templateSelect1, templateSelect2, templateSelect3];
+
+lvl.forEach((item, i) => {
+  const templateSelect = selectors[i];
+  const defaultOption = document.createElement('option');
+  defaultOption.value = '';
+  defaultOption.text = item;
+  defaultOption.selected = true;
+  defaultOption.disabled = true;
+  templateSelect.append(defaultOption);
+  const templates = solutions[item];
+
+  templates.forEach((template, index) => {
+    const option = document.createElement('option');
+    option.value = `${item} ${index} ${Object.keys(template)}`;
+    option.text = Object.keys(template);
+    templateSelect.append(option);
+  });
+
+  templateSelect.addEventListener('change', () => {
+    selectors.forEach((el) => {
+      const opt = el;
+      if (el !== selectors[i]) {
+        opt.value = '';
+      }
+    });
+    matrix = selectors[i].value;
+    if (item === 'medium') {
+      cellCount = 10;
+      titleSize = 70;
+    } else if (item === 'hard') {
+      cellCount = 15;
+      titleSize = 90;
+    } else {
+      cellCount = 5;
+      titleSize = 70;
+    }
+  });
+  controlsContainer.append(templateSelect);
 });
 
-controlsContainer.append(playBtn, restartBtn, templateSelect);
 canvasContainer.append(canvas, controlsContainer);
 const context = canvas.getContext('2d');
 
@@ -56,17 +89,16 @@ if (window.innerWidth < 950) {
 } else {
   canvasSize = 600;
 }
-
-canvas.width = canvasSize + TITLE_SIZE;
-canvas.height = canvasSize + TITLE_SIZE;
-let cellSize = canvasSize / cellCount;
-
+function canvasProperty() {
+  canvas.width = canvasSize + titleSize;
+  canvas.height = canvasSize + titleSize;
+  cellSize = canvasSize / cellCount;
+}
+canvasProperty();
 function resizeCanvas(size) {
   canvasSize = size;
-  cellSize = canvasSize / cellCount;
-  canvas.width = canvasSize + TITLE_SIZE;
-  canvas.height = canvasSize + TITLE_SIZE;
-  render(canvas, context, cellSize, gridColors, TITLE_SIZE, solutionArr);
+  canvasProperty();
+  render(canvas, context, cellSize, gridColors, titleSize, solutionArr);
 }
 function updateCanvasSize() {
   if (window.innerWidth <= 580) {
@@ -88,8 +120,8 @@ const handleMouseClick = (event) => {
     const mouseX = event.clientX - canvas.getBoundingClientRect().left;
     const mouseY = event.clientY - canvas.getBoundingClientRect().top;
 
-    const gridX = Math.floor((mouseX - TITLE_SIZE) / cellSize);
-    const gridY = Math.floor((mouseY - TITLE_SIZE) / cellSize);
+    const gridX = Math.floor((mouseX - titleSize) / cellSize);
+    const gridY = Math.floor((mouseY - titleSize) / cellSize);
 
     if (gridX >= 0 && gridY >= 0) {
       if (gridColors[gridX][gridY] === 0 || gridColors[gridX][gridY] === 2) {
@@ -98,7 +130,7 @@ const handleMouseClick = (event) => {
         gridColors[gridX][gridY] = 0;
       }
     }
-    render(canvas, context, cellSize, gridColors, TITLE_SIZE, solutionArr);
+    render(canvas, context, cellSize, gridColors, titleSize, solutionArr);
     const win = isMatrixEqual(gridColors, solutionArr);
     if (win) alert('You win!');
   }
@@ -109,8 +141,8 @@ const handleContextMenu = (event) => {
     const mouseX = event.clientX - canvas.getBoundingClientRect().left;
     const mouseY = event.clientY - canvas.getBoundingClientRect().top;
 
-    const gridX = Math.floor((mouseX - TITLE_SIZE) / cellSize);
-    const gridY = Math.floor((mouseY - TITLE_SIZE) / cellSize);
+    const gridX = Math.floor((mouseX - titleSize) / cellSize);
+    const gridY = Math.floor((mouseY - titleSize) / cellSize);
     if (gridX >= 0 && gridY >= 0) {
       if (gridColors[gridX][gridY] === 0 || gridColors[gridX][gridY] === 1) {
         gridColors[gridX][gridY] = 2;
@@ -118,13 +150,13 @@ const handleContextMenu = (event) => {
         gridColors[gridX][gridY] = 0;
       }
     }
-    render(canvas, context, cellSize, gridColors, TITLE_SIZE, solutionArr);
+    render(canvas, context, cellSize, gridColors, titleSize, solutionArr);
   }
 };
 const clearGrid = () => {
-  for (let i = 0; i < (canvas.width - TITLE_SIZE) / cellSize; i += 1) {
+  for (let i = 0; i < (canvas.width - titleSize) / cellSize; i += 1) {
     gridColors[i] = [];
-    for (let j = 0; j < (canvas.height - TITLE_SIZE) / cellSize; j += 1) {
+    for (let j = 0; j < (canvas.height - titleSize) / cellSize; j += 1) {
       gridColors[i][j] = 0;
     }
   }
@@ -132,11 +164,13 @@ const clearGrid = () => {
 
 function StartGame(value) {
   if (value) {
-    const arr = solutions[value.split(' ')[0]][value.split(' ')[1]];
+    canvasProperty();
+    const valueAddr = value.split(' ');
+    const arr = solutions[valueAddr[0]][valueAddr[1]][valueAddr[2]];
     gameBegin = true;
     solutionArr = arr;
     clearGrid();
-    render(canvas, context, cellSize, gridColors, TITLE_SIZE, arr);
+    render(canvas, context, cellSize, gridColors, titleSize, arr);
   }
 }
 
@@ -145,12 +179,12 @@ canvas.addEventListener('contextmenu', handleContextMenu);
 window.addEventListener('resize', updateCanvasSize);
 
 playBtn.addEventListener('click', () => {
-  StartGame(templateSelect.value);
+  StartGame(matrix);
 });
 restartBtn.addEventListener('click', () => {
   clearGrid();
-  render(canvas, context, cellSize, gridColors, TITLE_SIZE, solutionArr);
+  render(canvas, context, cellSize, gridColors, titleSize, solutionArr);
 });
 
 clearGrid();
-render(canvas, context, cellSize, gridColors, TITLE_SIZE, solutionArr);
+render(canvas, context, cellSize, gridColors, titleSize, solutionArr);
