599bc54bd1f685964290dbfc3f43cfadf5e5e741
mrrobot911 <kelwin2705@gmail.com>
2024-02-07 10:39:27 +0300

    fix: matrix equal

--- helpers/equal.js
+++ helpers/equal.js
@@ -1,7 +1,7 @@
 export default function isMatrixEqual(matrix1, matrix2) {
   for (let i = 0; i < matrix1.length; i += 1) {
     for (let j = 0; j < matrix1[i].length; j += 1) {
-      if (matrix1[i][j] !== 2 && matrix1[i][j] !== matrix2[i][j]) {
+      if (matrix2[i][j] === 1 && matrix1[i][j] !== matrix2[i][j]) {
         return false;
       }
     }
